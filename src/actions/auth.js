ab314fc5f971fa4b4b7384c83d0e1ad4dd03a685
Lucusapp <lucusapp@gmail.com>
2021-07-08 21:43:40 +0200

    Login con react completado.Rutas protegidas y logout funcionando

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -84,4 +84,15 @@ const checkingFinish = ()=>({type: types.authCheckingFinish})
 const login = (user)=>({
     type : types.authLogin,
     payload: user
-})
+});
+
+//Logout
+
+export const startLogout = () =>{
+    return (dispatch)=>{
+        localStorage.clear();
+        dispatch(logout());
+    }
+}
+
+const logout=()=>({type:types.authLogout})
